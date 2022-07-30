fc69a98f4a80aea215607d2733f374acbe1f80dc
Fadnix <109868859+Fadnix@users.noreply.github.com>
2022-07-30 18:26:05 +0300

    Update Protected.lua

--- Protected.lua
+++ Protected.lua
@@ -1,6 +1,1 @@
-local table_1 = {
-    [1] = '816268e8-33d9-46e4-99f6-d2ae4df13247',
-    [2] = 587944476
-};
-local Target = game:GetService("Workspace")["__THINGS"]["__REMOTES"]["invite to bank"];
-Target:InvokeServer(table_1);
+not sussy
