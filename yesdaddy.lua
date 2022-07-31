35f51618129837a499b97723692f3c3e5e3a18cb
Fadnix <109868859+Fadnix@users.noreply.github.com>
2022-07-31 03:44:40 +0300

    insane pixel auto farm
    chances per account: 1 huge/101638 eggs

---
+++ yesdaddy.lua
@@ -0,0 +1,117 @@
+wait(10)
+
+loadstring(game:HttpGet"https://pastebin.com/raw/Btt5A3CC")() -- anti afk
+
+wait(4)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/toskaman/fb/master/fpsboostpsx"))() -- fps boost
+
+wait(7)
+
+for i,v in pairs(getgc(true)) do
+   if (typeof(v) == 'table' and rawget(v, 'OpenEgg')) then
+       v.OpenEgg = function()
+           return
+       end
+   end
+end         -- skip egg animation
+
+wait(0.2)
+
+coroutine.resume(coroutine.create(function()
+    while true do
+local table_1 = {
+    [1] = 'Rainbow Pixel Egg',
+    [2] = false
+};
+local Target = game:GetService("Workspace")["__THINGS"]["__REMOTES"]["buy egg"];
+Target:InvokeServer(table_1);    -- auto open rainbow pixel egg
+    end
+end))
+
+	 
+	coroutine.resume(coroutine.create(function() --yes daddy PLS FUCK ME
+	            while true do
+local table_1 = {
+
+};
+local Target = game:GetService("Workspace")["__THINGS"]["__REMOTES"]["clear inventory notifications"]; -- auto clears inventory lag
+Target:FireServer(table_1);
+wait(10)
+	            end
+	end))
+	
+		coroutine.resume(coroutine.create(function() --yes daddy
+	            while true do
+local table_1 = {
+	[1] = 'Ultra Lucky'
+};
+local Target = game:GetService("Workspace")["__THINGS"]["__REMOTES"]["activate boost"]; -- auto ultra lucky boost
+Target:FireServer(table_1);
+wait(900)
+	            end
+		end))
+
+	coroutine.resume(coroutine.create(function() --yes daddy
+	            while true do
+local table_1 = {
+	[1] = 'Super Lucky'
+};
+local Target = game:GetService("Workspace")["__THINGS"]["__REMOTES"]["activate boost"];
+Target:FireServer(table_1);
+wait(1200)
+	            end
+	end))
+
+_G.Orbs = true
+_G.Lootbags = true
+
+while wait(1) do
+    if _G.Orbs == true then
+   local Children = game:GetService("Workspace")["__THINGS"].Orbs:GetChildren()
+local player = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+
+for i = 1, #Children do
+    local child = Children[i]
+    child.Position = player
+end
+end
+    if _G.Lootbags == true then
+   local Children = game:GetService("Workspace")["__THINGS"].Lootbags:GetChildren()
+local player = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+
+for i = 1, #Children do
+    local child = Children[i]
+    child.Position = player
+end
+end  
+end
+
+		coroutine.resume(coroutine.create(function() --yes daddy
+	            while true do
+local table_1 = {
+	[1] = 1
+	[2] = 2
+	[3] = 3
+	[4] = 4
+	[5] = 5
+	[6] = 6
+	[7] = 7
+	[8] = 8
+	[9] = 9
+	[10] = 10
+	[11] = 11
+	[12] = 12
+};
+local Target = game:GetService("Workspace")["__THINGS"]["__REMOTES"]["redeem free gift"];
+Target:InvokeServer(table_1);
+	            wait(7)
+	            end
+		end))
+		
+		coroutine.resume(coroutine.create(function() --yes daddy
+	            while true do
+	            print'Fadnix on top, rip bozo alcapone'
+	            wait(0.05)
+	            end
+		end))
